5e77753ecea914b8c9cae5e44b59a64415b40bda
Sdanisjb <31196541+Sdanisjb@users.noreply.github.com>
2018-06-13 08:20:55 -0500

    Add files via upload

---
+++ linkedLists/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include "linked_list.h"
+using namespace std;
+
+int main (){
+	clist miLista;
+	miLista.insert_element(5);
+	miLista.insert_element(3);
+	miLista.print();
+	miLista.insert_element(4);
+	
+	
+	
+	return 0;
+}
+
